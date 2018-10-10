5f1610bc74ba6514df214bfbb6ada7f8d7dfc508
yagi <h1201615004@hiratagakuen.onmicrosoft.com>
2018-10-10 11:16:13 +0900

    ｊ

--- Project1/Project1/ObjBlock.h
+++ Project1/Project1/ObjBlock.h
@@ -22,8 +22,7 @@ public:
 
 	int  GetMap(int x, int y);//マップ情報の取得
 	void  SetMap(int x, int y, int m) { m_map[y][x] = m; }
-	bool  GetRain() { return m_rain; }//雨の情報の取得
-	void  SetRain(bool r) { m_rain = r; }
+
 	
 
 private:
@@ -31,8 +30,7 @@ private:
 	float m_py;
 	float m_bx1;     //背景の動く値
 	float m_bx2;     //背景の動く値2
-	bool  m_rain;    //雨を降らす
-	
+
 	float m_scroll; //スクロール位置
 	float m_scroll_run; //ランナーの位置でスクを変えるようの変数
 
