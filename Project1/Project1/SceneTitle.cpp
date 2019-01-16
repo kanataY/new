d9c1c815612ff678b02cc873dc5709031accc7ef
yagi <h1201615004@hiratagakuen.onmicrosoft.com>
2019-01-16 16:30:20 +0900

    画像サイズの更新

--- Project1/Project1/SceneTitle.cpp
+++ Project1/Project1/SceneTitle.cpp
@@ -30,12 +30,21 @@ CSceneTitle::~CSceneTitle()
 //ゲームメイン初期化メソッド
 void CSceneTitle::InitScene()
 {
-	
+	Draw::LoadImageW(L"Image\\Title\\title1.png", 0, TEX_SIZE_1024);
+	Draw::LoadImageW(L"Image\\Title\\title2.png", 1, TEX_SIZE_1024);
+	Draw::LoadImageW(L"Image\\Title\\title3.png", 2, TEX_SIZE_1024);
+	Draw::LoadImageW(L"Image\\Title\\title4.png", 3, TEX_SIZE_1024);
+	Draw::LoadImageW(L"Image\\Title\\title5.png", 4, TEX_SIZE_1024);
+	Draw::LoadImageW(L"Image\\Title\\title6.png", 5, TEX_SIZE_1024);
+	Draw::LoadImageW(L"Image\\Title\\title7.png", 6, TEX_SIZE_1024);
+	Draw::LoadImageW(L"Image\\Title\\title8.png", 7, TEX_SIZE_1024);
+	Draw::LoadImageW(L"Image\\Title\\title9.png", 8, TEX_SIZE_1024);
+	Draw::LoadImageW(L"Image\\Title\\title10.png", 9, TEX_SIZE_1024);
 
 	//音楽情報の読み込み
 
-	//バックミュージックスタート
-	Audio::LoadAudio(0, L"BGM&SE\\BGMTitle_.wav", BACK_MUSIC);
+	////バックミュージックスタート
+	//Audio::LoadAudio(0, L"BGM&SE\\BGMTitle_.wav", BACK_MUSIC);
 
 	//音楽スタート
 	//Audio::Start(0);
