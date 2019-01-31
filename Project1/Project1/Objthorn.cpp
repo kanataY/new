9f11a7f5331f6b76d4330849d304c5cdf0bcc57a
yagi <h1201615004@hiratagakuen.onmicrosoft.com>
2019-01-31 11:59:25 +0900

    画像修正 針の画像の向きを出現場所によって変える

--- Project1/Project1/Objthorn.cpp
+++ Project1/Project1/Objthorn.cpp
@@ -16,7 +16,15 @@ CObjthorn::CObjthorn(float x, float y)
 //イニシャライズ
 void CObjthorn::Init()
 {
-	
+	m_vx = 0.0f;
+	m_vy = 0.0f;
+	//blockとの衝突状態確認用
+	m_hit_up = false;
+	m_hit_down = false;
+	m_hit_left = false;
+	m_hit_right = false;
+	m_pos = 0.0f;
+	m_block_type = 0; //踏んでいるblockの種類を確認用
 	//当たり判定用HitBoxを作成
 	Hits::SetHitBox(this, m_px, m_py, 64, 64, ELEMENT_ENEMY, OBJ_THORN, 1);
 
@@ -30,6 +38,22 @@ void CObjthorn::Action()
 	//HitBoxの位置の変更
 	CHitBox* hit = Hits::GetHitBox(this);
 	hit->SetPos(m_px + block->GetScroll(), m_py);
+
+	float c = m_px + block->GetScroll();
+
+	block->BlockHit(&c, &m_py, true,
+		&m_hit_up, &m_hit_down, &m_hit_left, &m_hit_right, &m_vx, &m_vy,
+		&m_block_type, false, false, 0, false, false
+	);
+
+	if (m_hit_up == true)
+	{
+		m_pos = 1.0f;
+	}
+	else
+	{
+		m_pos = 0.0f;
+	}
 }
 //ドロー
 void CObjthorn::Draw()
@@ -47,13 +71,22 @@ void CObjthorn::Draw()
 
 	//ブロック情報を持ってくる
 	CObjBlock* block = (CObjBlock*)Objs::GetObj(OBJ_BLOCK);
-
-	//表示位置の設定
-	dst.m_top = 0.0f + m_py;
-	dst.m_left = 64.0f + m_px + block->GetScroll();
-	dst.m_right = 0.0f + m_px + block->GetScroll();
-	dst.m_bottom = 64.0f + m_py;
-
+	if (m_pos == 0.0f)
+	{
+		//表示位置の設定
+		dst.m_top = 0.0f + m_py;
+		dst.m_left = 64.0f + m_px + block->GetScroll();
+		dst.m_right = 0.0f + m_px + block->GetScroll();
+		dst.m_bottom = 64.0f + m_py;
+	}
+	else
+	{
+		//表示位置の設定
+		dst.m_top = 64.0f + m_py;
+		dst.m_left = 64.0f + m_px + block->GetScroll();
+		dst.m_right = 0.0f + m_px + block->GetScroll();
+		dst.m_bottom = 0.0f + m_py;
+	}
 	//10番目に登録したグラフィックをsrc・dst・c・の情報を元に描画
 	Draw::Draw(10, &src, &dst, c, 0.0);
 }
