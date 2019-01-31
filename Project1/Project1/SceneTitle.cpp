c2f6c64bdfce5dca4beb77797e6819c08bfc24ac
nakano <h1201615016@hiratagakuen.onmicrosoft.com>
2019-01-31 15:41:56 +0900

    メインの画像追加

--- Project1/Project1/SceneTitle.cpp
+++ Project1/Project1/SceneTitle.cpp
@@ -53,7 +53,7 @@ void CSceneTitle::InitScene()
 	//音楽情報の読み込み
 
 	////バックミュージックスタート
-	Audio::LoadAudio(0, L"BGM&SE\\syareotu.wav", BACK_MUSIC);
+	Audio::LoadAudio(0, L"BGM&SE\\main1.wav", BACK_MUSIC);
 
 	//効果音
 	Audio::LoadAudio(1, L"BGM&SE\\pinponn2.wav", EFFECT);		//カーソルを動かしたときの音
